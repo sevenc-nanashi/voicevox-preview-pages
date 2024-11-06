27922a795993c8fa8d0caaa92c03f7e4bd968519
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-11-06 17:20:11 +0900

    Fix: BASE_URLの後にスラッシュは要らない

--- src/composables/useDownloadData.ts
+++ src/composables/useDownloadData.ts
@@ -20,7 +20,7 @@ export type DownloadData = {
 
 const downloadDataRef = ref<DownloadData[] | null>(null);
 
-void fetch(`${import.meta.env.BASE_URL}/preview/downloads.json`).then(
+void fetch(`${import.meta.env.BASE_URL}preview/downloads.json`).then(
   async (response) => {
     if (!response.ok) {
       throw new Error(`Failed to fetch downloads.json: ${response.statusText}`);
